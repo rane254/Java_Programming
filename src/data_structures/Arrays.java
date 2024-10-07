c4f7d6df9caf749bb366eb905b2455dcdaee32f7
rane254 <mr.appmaker.dev@gmail.com>
2024-10-07 15:15:02 +0530

    arrays - homework problems

--- src/data_structures/Arrays.java
+++ src/data_structures/Arrays.java
@@ -8,7 +8,9 @@ public class Arrays  {
         // type[] arrayName = {1, 2, 3, 4, 5};
 
         // printMarks();
-        linearSearchAlgo();
+        // linearSearchAlgo();
+        // findMinMaxNum();
+        isAscendingArr();
     }
 
     public static void printMarks() {
@@ -51,6 +53,64 @@ public class Arrays  {
                 System.out.println("Number " + num + " found at index " + i);
                 break;
             }
+        }
+    }
+
+    public static void findMinMaxNum() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of an array/list: ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        for (int i=0; i<size; i++) {
+            System.out.print("Enter number " + (i+1) + " : ");
+            arr[i] = sc.nextInt();
+        }
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int ele : arr) {
+            if (ele > max) {
+                max = ele;
+            }
+
+            if (ele < min) {
+                min = ele;
+            }
+        }
+
+        System.out.println("Maximum: " + max + "\nMinimum: " + min);
+    }
+
+    public static void isAscendingArr() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of an array/list: ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        for (int i=0; i<size; i++) {
+            System.out.print("Enter number " + (i+1) + " : ");
+            arr[i] = sc.nextInt();
+        }
+
+        boolean isAscending = true;
+
+        for (int i=0; i<size-1; i++) {
+            if (arr[i] > arr[i+1]) {
+                isAscending = false;
+                break;
+            }
+        }
+
+        if (isAscending) {
+            System.out.println("The arr is in ascending order.");
+        } else {
+            System.out.println("The arr is not in ascending order.");
         }
     }
 }

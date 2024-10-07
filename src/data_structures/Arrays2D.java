d5f5d38074987d25681558fd2d4c9df0128593a4
rane254 <mr.appmaker.dev@gmail.com>
2024-10-07 17:33:41 +0530

    2d array - linearSearch

--- src/data_structures/Arrays2D.java
+++ src/data_structures/Arrays2D.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Arrays2D {
     public static void main(String[] args) {
-        print2DNumArr();
+        // print2DNumArr();
+        linearSearchMatrix();
     }
 
     public static void print2DNumArr() {
@@ -38,6 +39,43 @@ public class Arrays2D {
                 System.out.print(arr[i][j] + ", ");
             }
             System.out.println();
+        }
+    }
+
+    public static void linearSearchMatrix() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the row size of an 2D Array: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter the column size of an 2D Array: ");
+        int cols = sc.nextInt();
+
+        int[][] arr = new int[rows][cols];
+
+        System.out.println("Enter the number");
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<cols; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.print("Enter the number to search in an 2D Array/Matrix: ");
+        int num = sc.nextInt();
+
+        boolean isFound = false;
+        System.out.println("------------O/p----------");
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<cols; j++) {
+                if (arr[i][j] == num) {
+                    isFound = true;
+                    System.out.print("[" + i + "," + j + "] : " + num);
+                    break;
+                }
+            }
+        }
+
+        if (!isFound) {
+            System.out.print(num + " not found in the above matrix!");
         }
     }
 }

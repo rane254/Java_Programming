df3735a4d6549b0decaf8954d55528835c049fd2
rane254 <mr.appmaker.dev@gmail.com>
2024-10-07 17:06:20 +0530

    arrays - 2d array

---
+++ src/data_structures/Arrays2D.java
@@ -0,0 +1,34 @@
+package data_structures;
+
+import java.util.Scanner;
+
+public class Arrays2D {
+    public static void main(String[] args) {
+        print2DNumArr();
+    }
+
+    public static void print2DNumArr() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the row size of an 2D Array: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter the column size of an 2D Array: ");
+        int cols = sc.nextInt();
+
+        int[][] arr = new int[rows][cols];
+        int cnt = 1;
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<cols; j++) {
+                System.out.print("Enter number " + (cnt++) + " : ");
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("------------O/p----------");
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<cols; j++) {
+                System.out.println("Index [" + i + "," + j + "] : " + arr[i][j]);
+            }
+        }
+    }
+}

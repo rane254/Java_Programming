5eba2ca1e993213ade08890687b2aa9096c927e2
rane254 <mr.appmaker.dev@gmail.com>
2024-10-08 15:09:33 +0530

    Update bit manipulation using bit mask

--- src/operators/NumberSystem.java
+++ src/operators/NumberSystem.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class NumberSystem {
     public static void main(String[] args) {
         // bitwiseLRShiftOperators();
-        bitManipulation();
+        // bitManipulation();
+        updateBit();
     }
 
     public static void bitwiseLRShiftOperators() {
@@ -40,6 +41,41 @@ public class NumberSystem {
             System.out.println("Bit was zero.");
         } else {
             System.out.println("Bit was non-zero.");
+        }
+    }
+
+    public static void updateBit() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+
+        String binNum = Integer.toBinaryString(n);
+        System.out.println("Binary number of " + n + " is " + binNum);
+
+        System.out.print("Enter the position of the bit: ");
+        int pos = sc.nextInt();
+
+        int bitMask = 1 << pos;
+        System.out.println(bitMask);
+        System.out.println(Integer.toBinaryString(bitMask));
+
+        if (pos > binNum.length() || pos < 0) {
+            System.out.println("Position out of bound!");
+            return;
+        }
+
+        System.out.println("Enter 1 or 0 to update: ");
+        int op = sc.nextInt();
+
+        if (op == 1) {
+            String finalBitMask = Integer.toBinaryString(n & (~bitMask));
+
+            System.out.println("Binary number after AND operation on ~ Bit Mask: " + finalBitMask);
+        } else {
+            String finalBitMask = Integer.toBinaryString(bitMask | n);
+
+            System.out.println("Binary number of " + (bitMask | n) + " after OR operation on Bit Mask: " + finalBitMask);
         }
     }
 }

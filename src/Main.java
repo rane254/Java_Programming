f668cc0839836e67006b65eba44428a0f27bea78
rane254 <mr.appmaker.dev@gmail.com>
2024-10-15 19:44:37 +0530

    updates

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // System.out.println('0' * 1); // ASCII Value
 
         Scanner sc = new Scanner(System.in);
+
+// Initializes objects to read user input from the console.
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(in);
+        String input = br.readLine();
+
 
 //        int a = sc.nextInt();
 //        int b = sc.nextInt();
@@ -15,7 +24,7 @@ public class Main {
 //        String name = sc.nextLine();
 //
 //        System.out.println("O/p => " + name);
-
+        greet(input);
 
     }
 

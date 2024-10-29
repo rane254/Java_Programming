19ab9116cdcc75b6e9207f3cb97effc8f876345b
rane254 <mr.appmaker.dev@gmail.com>
2024-10-30 01:18:33 +0530

    Kunal Khushwaha - 744 - Arrays & Binary Search

---
+++ src/leetcode/arrays/NextGreatestLetter.java
@@ -0,0 +1,27 @@
+package leetcode.arrays;
+
+import java.util.Arrays;
+
+public class NextGreatestLetter {
+    public static void main(String[] args) {
+        char[] letters = {'c','f','j'};
+        char ans = nextGreatestLetter(letters, 'f');
+        System.out.println(ans);
+    }
+
+    static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length-1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return letters[start % letters.length];
+    }
+}

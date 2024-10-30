bc6cd3aad5eb4c7873f69ae023b7c49d0d7ed6df
rane254 <mr.appmaker.dev@gmail.com>
2024-10-30 19:04:41 +0530

    Kunal Khushwaha - Basic Sorts

---
+++ src/leetcode/sorts/CyclicSort.java
@@ -0,0 +1,40 @@
+package leetcode.sorts;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        // This sort works only when the elements are 1-N in an array.
+        int[] arr = {3, 2, 1, 5, 4};
+        System.out.println(Arrays.toString(arr));
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+//        int ans = firstMissingPositive(arr);
+//        System.out.println(ans);
+
+    }
+
+    public static void cyclicSort(int[] arr) {
+        int i=0;
+        while (i < arr.length) {
+            int correctValueIndex = arr[i] -1;
+            if (arr[i] >= arr[correctValueIndex]) {
+                swap(arr, i, correctValueIndex);
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int i, int correctValueIndex) {
+        int temp = arr[i];
+        arr[i] = arr[correctValueIndex];
+        arr[correctValueIndex] = temp;
+    }
+
+
+    static int firstMissingPositive(int[] nums) {
+        int ans = 0;
+
+        return ans;
+    }
+}

6a5753ab67b8476beecfa25ec76e73a5acda8780
rane254 <mr.appmaker.dev@gmail.com>
2024-11-02 00:55:43 +0530

    Kunal Khushwaha - Recursion - Quick Sort

--- src/concepts/Recursion.java
+++ src/concepts/Recursion.java
@@ -62,3 +62,43 @@ class MergeSort {
         return mergedArr;
     }
 }
+
+class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 2, 4, 3};
+        int n = arr.length;
+        quickSort(arr, 0, n-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIdx = partition(arr, low, high);
+
+            quickSort(arr, low, pivotIdx-1);
+            quickSort(arr, pivotIdx+1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high]; // taking last element
+        int i = low-1; //create empty space
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+
+        return i;
+    }
+}

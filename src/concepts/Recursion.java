63547d9cfc295af6d747180f9e6d8000b22f3f14
rane254 <mr.appmaker.dev@gmail.com>
2024-11-01 22:30:50 +0530

    Kunal Khushwaha - Recursion - Merge Sort

--- src/concepts/Recursion.java
+++ src/concepts/Recursion.java
@@ -1,8 +1,11 @@
 package concepts;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Recursion {
     public static void main(String[] args) {
-        message(0);
+//        message(0);
     }
 
     static void message(int i) {
@@ -13,4 +16,49 @@ public class Recursion {
         message(i+1);
     }
 
+}
+
+class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 2, 4, 3};
+        int[] ans = mergeSort(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] mergeSort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+
+        int mid = arr.length / 2;
+
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
+
+    private static int[] merge(int[] first, int[] second) {
+        int[] mergedArr = new int[first.length + second.length];
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < first.length  && j < second.length) {
+            if (first[i] < second[j]) {
+                mergedArr[k++] = first[i++];
+            } else {
+                mergedArr[k++] = second[j++];
+            }
+        }
+
+        while (i < first.length) {
+            mergedArr[k++] = first[i++];
+        }
+
+        while (j < second.length) {
+            mergedArr[k++] = second[j++];
+        }
+
+        return mergedArr;
+    }
 }

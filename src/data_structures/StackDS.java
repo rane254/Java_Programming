5feea1d8d8f87fa00cfcf615230dee8a97f3881d
rane254 <mr.appmaker.dev@gmail.com>
2024-11-03 16:44:11 +0530

    Apna College - OOPS - Stack - push, pop, peek, getSize, printStack operations

---
+++ src/data_structures/StackDS.java
@@ -0,0 +1,93 @@
+package data_structures;
+
+public class StackDS {
+    public static Node head;
+    private int size;
+
+    StackDS() {
+        this.size = 0;
+    }
+    public class Node {
+        int data;
+        Node next;
+
+        Node (int data) {
+            this.data = data;
+            this.next = null;
+            size++;
+        }
+    }
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void pop() {
+        if (head == null) {
+            System.out.println("Stack is empty!");
+            return;
+        }
+
+        System.out.println("Popped: " + head.data);
+
+        head = head.next;
+        size--;
+    }
+
+    public void peek() {
+        if (head == null) {
+            System.out.println("Stack is empty!");
+            return;
+        }
+
+        System.out.println(head.data);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void printStack() {
+        if (head == null) {
+            System.out.println("Stack is empty!");
+            return;
+        }
+
+        System.out.println("---------TOP--------");
+
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.println(currentNode.data);
+            currentNode = currentNode.next;
+        }
+
+        System.out.println("--------BOTTOM------");
+    }
+
+    public static void main(String[] args) {
+        StackDS stack = new StackDS();
+        stack.push(1);
+        stack.peek();
+        stack.push(3);
+        stack.push(5);
+        stack.peek();
+        stack.printStack();
+        System.out.println(stack.getSize());
+        stack.pop();
+        stack.printStack();
+        System.out.println(stack.getSize());
+        stack.pop();
+        stack.pop();
+        stack.printStack();
+        System.out.println(stack.getSize());
+        stack.pop();
+    }
+}
